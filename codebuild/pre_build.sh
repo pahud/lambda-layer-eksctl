39e3e467a08925a8b0be3d7572149009be38e3a3
Pahud Hsieh <pahudnet@gmail.com>
2019-11-14 09:30:32 +0800

    Update pre_build.sh

--- codebuild/pre_build.sh
+++ codebuild/pre_build.sh
@@ -15,7 +15,7 @@ cat << EOF > ~/.aws/config
 [default]
 EOF
 
-# aws configure --profile default set aws_access_key_id ${AWS_ACCESS_KEY_ID}
-# aws configure --profile default set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-# aws configure --profile default set aws_session_token ${AWS_SESSION_TOKEN}
+aws configure --profile default set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+aws configure --profile default set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+aws configure --profile default set aws_session_token ${AWS_SESSION_TOKEN}
 
