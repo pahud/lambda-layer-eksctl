65e79d2add6f31b7b1dc33db20004378a25e6ecb
Pahud Hsieh <pahudnet@gmail.com>
2019-11-14 09:14:05 +0800

    Update pre_build.sh

--- codebuild/pre_build.sh
+++ codebuild/pre_build.sh
@@ -6,6 +6,14 @@ echo "AWS_ACCESS_KEY_ID=$(cat session.json | jq -r .Credentials.AccessKeyId)" > 
 echo "AWS_SECRET_ACCESS_KEY=$(cat session.json | jq -r .Credentials.SecretAccessKey)" >> envfile
 echo "AWS_SESSION_TOKEN=$(cat session.json | jq -r .Credentials.SessionToken)" >> envfile
 source envfile
+
+echo "generating the aws cli default profile"
+mkdir ~/.aws
+
+cat << EOF > ~/.aws/config
+[default]
+EOF
+
 aws configure --profile default set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure --profile default set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 aws configure --profile default set aws_session_token ${AWS_SESSION_TOKEN}
