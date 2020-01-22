8967fdc1da7e6601d7d7b591132050236e2a90d9
Pahud Hsieh <pahudnet@gmail.com>
2020-01-22 23:03:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 
 TAG='eksctl:latest'
 
-docker build -t $TAG .
+docker build -t $TAG . --build-arg DOCKER_MIRROR=${DOCKER_MIRROR-''}
+
 CONTAINER=$(docker run -d $TAG false)
 docker cp ${CONTAINER}:/layer.zip layer.zip
 docker cp ${CONTAINER}:/VERSION VERSION
